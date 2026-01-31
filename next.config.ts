4f3f58200ff4ce39c5651d55f483ddba81334c04
jann5 <102853903+jann5@users.noreply.github.com>
2026-01-31 16:09:56 +0100

    Fix assetPrefix for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'dist',
+  assetPrefix: '/wizyt-wka-drukmajster3d',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
